e26f092613bcaee698acd0edf39dc7eb54d70f4d
eddy <edwardquintero@protonmail.com>
2025-07-24 01:39:37 -0400

    "fix"

--- src/lib/convex.ts
+++ src/lib/convex.ts
@@ -1,6 +1,6 @@
 // Re-export convex types and utilities for use in components
-import type { Id } from "../../convex/_generated/dataModel";
+import type { Id, Doc } from "../../convex/_generated/dataModel";
 import { api } from "../../convex/_generated/api";
 
-export type { Id };
+export type { Id, Doc };
 export { api };
