a3eef3bd391dbf7be99deac9bcd04d2d1a61c57f
Daniel Velasco <divelasco7@gmail.com>
2025-07-25 23:28:22 -0500

    feat: more http routes

--- convex/http.ts
+++ convex/http.ts
@@ -1,4 +1,5 @@
 import { httpRouter } from "convex/server";
+import { getAuthSessionId, getAuthUserId } from "@convex-dev/auth/server";
 import { auth } from "./auth";
 import { httpAction } from "./_generated/server";
 import { api } from "./_generated/api";
@@ -20,6 +21,16 @@ const requireDevApiKey = (handler: (ctx: any, request: Request) => Promise<Respo
   });
 };
 
+const requireJWT = (handler: (ctx: any, request: Request) => Promise<Response>) => {
+  return httpAction(async (ctx, request) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      return new Response("Unauthorized", { status: 403 })
+    }
+    return handler(ctx, request);
+  });
+};
+
 http.route({
   path: "/health",
   method: "GET",
@@ -29,19 +40,22 @@ http.route({
 });
 
 http.route({
-  path: "/user",
+  path: "/users",
   method: "GET",
-  handler: requireDevApiKey(async (ctx, request) => {
-    const { searchParams } = new URL(request.url);
-    const username = searchParams.get("username");
-
-    if (!username) {
-      return new Response("Missing `username` parameter", { status: 400 });
-    }
-
-    const user = await ctx.runQuery(api.main.findUserByUsername, {
-      username: username,
+  handler: requireDevApiKey(async (ctx) => {
+    const userCount = await ctx.runQuery(api.main.getUserCount);
+    return new Response(JSON.stringify({ totalUsers: userCount }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
     });
+  }),
+});
+
+http.route({
+  path: "/api/user",
+  method: "GET",
+  handler: requireJWT(async (ctx, request) => {
+    const user = await ctx.runQuery(api.main.getMyUserProfile);
 
     if (!user) {
       return new Response("User not found", { status: 404 });
@@ -60,22 +74,90 @@ http.route({
 });
 
 http.route({
-  path: "/users",
+  path: "/api/user/notifications",
   method: "GET",
-  handler: requireDevApiKey(async (ctx) => {
-    const userCount = await ctx.runQuery(api.main.getUserCount);
-    return new Response(JSON.stringify({ totalUsers: userCount }), {
+  handler: requireJWT(async (ctx, request) => {
+    const notifications = await ctx.runQuery(api.notifications.getNotifications);
+
+    return new Response(JSON.stringify(notifications), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
   }),
 });
 
+http.route({
+  path: "/api/teams",
+  method: "GET",
+  handler: requireJWT(async (ctx, request) => {
+    const teams = await ctx.runQuery(api.teams.getTeams);
+
+    return new Response(JSON.stringify(teams), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  }),
+});
+
+http.route({
+  path: "/api/teams",
+  method: "POST",
+  handler: requireJWT(async (ctx, request) => {
+    const { teamName } = await request.json();
+
+    if (typeof teamName !== "string") {
+      return new Response("Invalid request body", { status: 400 });
+    }
+    const team = await ctx.runMutation(api.teams.createTeam, {
+      name: teamName
+    });
+
+    return new Response(JSON.stringify(team), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  }),
+});
+
+http.route({
+  pathPrefix: "/api/teams/",
+  method: "GET",
+  handler: httpAction(async (ctx, request) => {
+    const [teamId, ...subpath] = new URL(request.url).pathname.slice("/api/teams/".length).split("/");
+
+    if (!teamId || (subpath[0] !== "lists" && subpath[0] !== "members")) {
+      return new Response("Not Found", { status: 404 });
+    }
+
+    if (subpath[0] === "lists") {
+      /*
+      const lists = await ctx.runQuery(api.teams.getLists);
+      return new Response(JSON.stringify(lists), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+      */
+      return new Response("Not Found", { status: 404 });
+    }
+
+    if (subpath[0] === "members") {
+      const lists = await ctx.runQuery(api.teams.getTeamMembers, {
+        teamId: teamId as Id<"teams">
+      });
+      return new Response(JSON.stringify(lists), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    return new Response("Internal Server Error", { status: 500 });
+  }),
+});
 
 http.route({
   path: "/lists",
   method: "GET",
-  handler: httpAction(async (ctx, request) => {
+  handler: requireJWT(async (ctx, request) => {
     const { searchParams } = new URL(request.url);
 
     const lists = await ctx.runQuery(api.lists.getLists);
